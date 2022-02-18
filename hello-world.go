3ff44d7b626dc40788887c5dacb6a8190bf179a6
AlexF4Dev <alex.furman@labshare.org>
2022-02-18 16:22:16 -0500

    updated the app

--- hello-world.go
+++ hello-world.go
@@ -1,7 +1,15 @@
 package main
-
-import "fmt"
-
+import (
+	"fmt"
+	"net/http"
+)
 func main() {
-	fmt.Println("Hello from Docker on GitHub Container Registry!\nThis message shows that your installation appears to be working correctly.\n\nAs Docker Inc introduced rate-limiting in https://www.docker.com/increase-rate-limits\nwe simply need our own hello-world image on GitHub Container Registry.\n\nTo generate this message, Docker took the following steps:\n 1. The Docker client contacted the Docker daemon.\n 2. The Docker daemon pulled this \"hello-world\" image from the GitHub Container Registry.\n    (amd64)\n 3. The Docker daemon created a new container from that image which runs the\n    executable that produces the output you are currently reading.\n 4. The Docker daemon streamed that output to the Docker client, which sent it\n    to your terminal.\n\n")
+	handler := http.NewServeMux()
+	handler.HandleFunc("/", SayHello)
+	handler.HandleFunc("/api/hello", SayHello)
+	http.ListenAndServe("0.0.0.0:8080", handler)
 }
+func SayHello(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, `Hello world`)
+}
+
